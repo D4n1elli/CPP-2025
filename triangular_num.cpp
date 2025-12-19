2d2d263c22295ce4d236e012438dde07d3915d9f
Danielli <d4nielli.dev@gmail.com>
2025-12-18 21:35:22 -0300

    structure
    Made the basic structure for the code

---
+++ triangular_num.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// Made in 18/12/2025
+
+bool is_triangular(int num){
+    for(int i = 1; i>= num; i++){
+        sum += i;
+    }
+}
+
+
+
+int main(){
+    cout << "Triangular numbers\n";
+    cout << "Type a number to know if it is triangular!\n";
+
+    int number; cin >> number;
+
+}
